babcf8fcd89de460caf790de02c8bd18f8955c02
이건우 <geonwoo0223@gmail.com>
2021-01-22 16:33:00 +0900

    Create Frontend Project
     - 프론트엔드 프로젝트 생성

---
+++ frontend/src/components/common/Nav2.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="Nav">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light row">
+        <div class="col-5">
+            <a class="navbar-brand" href="#">로고</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        <div class="col-3">
+            2
+        </div>
+        <div class="col-1">
+            3
+        </div>
+        <div class="col-3">
+            4
+        </div>
+
+    </nav>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
