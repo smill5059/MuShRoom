d810ed86aa05588a1d2d356cce53cc81bc885554
권연욱 <w00k0805@naver.com>
2021-01-25 20:26:15 +0900

    Create components for Main Page
    - src/views/Home.vue: 메인화면 수정
    - src/components/common/Footer.vue: footer 컴포넌트생성
    - src/components/class/ClassList.vue: 강의리스트 조회 컴포넌트 생성
    - src/views/About.vue: vue-cli 초기 파일 삭제
    - src/components/Helloworld.vue: vue-cli 초기 파일 삭제

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <v-carousel v-model="model"
+    height=300
+    cycle
+    hide-delimiter-background
+    show-arrows-on-hover>
+      <v-carousel-item
+        v-for="(color, i) in colors"
+        :key="color"
+      >
+        <v-sheet
+          :color="color"
+          height="100%"
+          tile
+        >
+          <v-row
+            class="fill-height"
+            align="center"
+            justify="center"
+          >
+            <div class="display-3">
+              Slide {{ i + 1 }}
+            </div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+    <div v-for="title in titles"
+    :key="title">
+      <ClassList :name="title" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import ClassList from '@/components/class/ClassList.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    ClassList
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  private model = 0;
+  private colors: string[] = [
+    'primary',
+    'secondary',
+    'yellow darken-2',
+    'red',
+    'orange',
+  ];
+  private titles: string[] =[
+    'Top 10 인기 강의',
+    '신규 강의',
+    '꾸준히 사랑받는 강의'
+  ]
+}
 </script>
