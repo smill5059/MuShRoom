609ac96f7a11ad7259c55bf1ea42d0ad22063820
권연욱 <w00k0805@naver.com>
2021-01-26 02:14:50 +0000

    Feature/navbar

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <Nav/>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+import Nav from './components/common/Nav2.vue';
+
+export default Vue.extend({
+  name: 'App',
+
+  components: {
+    HelloWorld,
+    Nav
+  },
+
+  data: () => ({
+    //
+  }),
+});
+</script>
