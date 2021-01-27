80dd7395f4dfeb8b08647aecf1124931fd705e08
이병희 <milk1543@naver.com>
2021-01-27 21:50:27 +0900

    Add Controller, Model, Repository
    - backend/controller/TutorController.java
      - REST API 구현
        - GET /tutor
        - POST /tutor
        - GET /tutor/{id}
        - PUT /tutor/{id}
        - DELETE /tutor/{id}
        - GET /tutor/{id}/feedback
        - POST /tutor/{id}/feedback
      - removeDuplicateCareerByName : 경력 리스트간 중복 제거 메소드 추가
      - removeDuplicateAwardByName : 수상 리스트간 중복 제거 메소드 추가
    - backend/model/Profile.java : Profile 모델 추가
    - backend/model/Tutor.java : Tutor Entity 모델 추가
    - backend/model/TutorAccount.java : TutorAccount 모델 추가
    - backend/model/TutorAward.java : TutorAward 모델 추가
    - backend/model/TutorCareer.java : TutorCareer 모델 추가
    - backend/model/TutorClass.java : TutorClass 모델 추가
    - backend/model/TutorFeedbackAvailableTime.java
      - TutorFeedbackAvailableTime 모델 추가
    - backend/model/TutorFeedbackReservationTime.java
      - TutorFeedbackReservationTime 모델 추가
    - backend/repository/TutorRepository
      - Optional<Tutor> findById(ObjectId id) 추가
      - List<Tutor> findAll() 추가

--- backend/src/main/java/com/ssafy/backend/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.backend.repository;
-
-import com.ssafy.backend.model.User;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, ObjectId> {
-    public User findByEmail(String email);
-}
