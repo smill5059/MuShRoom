107df7b6594a50dd4274e0f52e55bdf224db7904
정수림 <smill5059@naver.com>
2021-01-28 10:27:48 +0900

    Update user controller
    - backend/controller/UserController.java
     - 회원정보 수정 repository에서 id로 찾아서 수정할 수 있게 수정
     - 비밀번호 변경, 비밀번호 찾기, 회원탈퇴 추가
    - backend/util/: MailSender.java, PasswordUtil.java 추가
    - backend/pom.xml: smtp dependency 추가
    - backend/application.properties: stmp 관련 추가

--- backend/src/main/java/com/ssafy/backend/controller/UserController.java
+++ backend/src/main/java/com/ssafy/backend/controller/UserController.java
@@ -2,9 +2,15 @@ package com.ssafy.backend.controller;
 
 import com.ssafy.backend.model.User;
 import com.ssafy.backend.repository.UserRepository;
+import com.ssafy.backend.util.MailSender;
+import com.ssafy.backend.util.PasswordUtil;
 import com.ssafy.backend.util.UserSha256;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -15,15 +21,14 @@ public class UserController {
     @Autowired
     private UserRepository repository;
 
-    @ApiOperation(value="회원 가입")
+    @ApiOperation(value = "회원 가입")
     @PostMapping("/signup")
     public void signup(@RequestBody User user) {
-//        repository.insert(new User("ssafy@ssafy.com", "ssafy", "010-0000-0000", "김싸피", 0));
         user.setPassword(UserSha256.encrypt(user.getPassword()));
         repository.insert(user);
     }
 
-    @ApiOperation(value="로그인")
+    @ApiOperation(value = "로그인")
     @PostMapping("/login")
     public User login(@RequestBody User user) {
         User findUser = repository.findByEmail(user.getEmail());
@@ -39,19 +44,61 @@ public class UserController {
         }
     }
 
-    @ApiOperation(value="회원 정보 수정")
-    @PostMapping("/update")
-    public void updateUserInfo(@RequestBody User user) {
-        repository.save(user); //덮어씌우므로 user에 모든 정보가 포함되어야 함...
+    @ApiOperation(value = "회원 정보 수정")
+    @PostMapping("/updateInfo")
+    public void updateInformation(@RequestBody User user) {
+        Optional<User> findUser = repository.findById(user.getId());
+        if (user.getPhone() != null) findUser.get().setPhone(user.getPhone());
+        if (user.getNickname() != null) findUser.get().setNickname(user.getNickname());
+        if (user.getProfile() != null) findUser.get().setProfile(user.getProfile());
+        if (user.getIntroduction() != null) findUser.get().setIntroduction(user.getIntroduction());
+        if (user.getInstrument() != null) findUser.get().setInstrument(user.getInstrument());
+        repository.save(findUser.get());
     }
 
-    /* 이상이상....
-    @ApiOperation(value="회원 정보 조회")
-    @PostMapping("/")
-    public User findById(@RequestBody User user) {
-        Optional<User> op = repository.findById(user.getId());
-        return op.get();
+    @ApiOperation(value = "비밀번호 변경")
+    @PostMapping("/updatePwd")
+    public void updatePassword(@RequestBody User user) {
+        Optional<User> findUser = repository.findById(user.getId());
+        findUser.get().setPassword(UserSha256.encrypt(user.getPassword()));
+        repository.save(findUser.get());
     }
-    */
+
+    @Autowired
+    public JavaMailSender javaMailSender;
+
+    public void sendMail(String email, String pwd) {
+        SimpleMailMessage s = new SimpleMailMessage();
+        s.setTo(email);
+        s.setSubject("MuShRoom 비밀번호 찾기");
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("임시 비밀번호: ").append(pwd).append("\n");
+        sb.append("보안을 위해서 로그인 후 바로 비밀번호를 변경해주세요.");
+
+        s.setText(sb.toString());
+        javaMailSender.send(s);
+    }
+
+    @ApiOperation(value = "비밀번호 찾기")
+    @PostMapping("/findPwd")
+    public void findPassword(@RequestBody User user) {
+        String newPassword = PasswordUtil.getRandomPassword(8);
+//        System.out.println(newPassword);
+//        System.out.println(UserSha256.encrypt(newPassword));
+        User findUser = repository.findByEmail(user.getEmail());
+        findUser.setPassword(UserSha256.encrypt(newPassword));
+        repository.save(findUser);
+
+        sendMail(findUser.getEmail(), newPassword);
+    }
+
+    @ApiOperation(value = "회원 탈퇴")
+    @PostMapping("/withdraw/{id}")
+    public void withdrawal(@PathVariable ObjectId id) {
+        Optional<User> findUser = repository.findById(id);
+        repository.delete(findUser.get());
+//        repository.delete(user);
+    }
 
 }
