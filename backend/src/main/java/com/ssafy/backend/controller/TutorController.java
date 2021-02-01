707adfdfb033af07852bf763cca5629308e3052b
이병희 <milk1543@naver.com>
2021-02-01 14:38:53 +0900

    Update TutorController, Tutor
    - backend/controller/TutorController.java
      - REST API 추가 및 개선
      - 코드 가시성 개선
      - 예외 처리 추가
    - backend/model/Tutor.java
      - Tutor형 변수를 받는 생성자 개선

--- backend/src/main/java/com/ssafy/backend/controller/TutorController.java
+++ backend/src/main/java/com/ssafy/backend/controller/TutorController.java
@@ -1,6 +1,7 @@
 package com.ssafy.backend.controller;
 
 import com.ssafy.backend.model.ClassEntity;
+import com.ssafy.backend.model.ErrorMessage;
 import com.ssafy.backend.model.TutorAccount;
 import com.ssafy.backend.model.TutorAward;
 import com.ssafy.backend.model.TutorCareer;
@@ -9,16 +10,20 @@ import com.ssafy.backend.model.Profile;
 import com.ssafy.backend.model.Tutor;
 import com.ssafy.backend.model.TutorFeedbackAvailableTime;
 import com.ssafy.backend.repository.TutorRepository;
+import com.ssafy.backend.util.ErrorType;
 import com.ssafy.backend.util.UserSha256;
 import io.swagger.annotations.ApiOperation;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import org.apache.tomcat.util.http.parser.HttpParser;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,143 +34,134 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/")
 public class TutorController {
 
   // @Autowired
   // private ClassRepository classRepository;
   @Autowired
-  private TutorRepository tutorRepository;
+  private final TutorRepository tutorRepository;
 //   @Autowired
 //   private InstrumentRepository instrumentRepository;
 
+  public TutorController(TutorRepository tutorRepository) {
+    this.tutorRepository = tutorRepository;
+  }
+
   @ApiOperation(value = "Get All Tutor List", notes = "모든 튜터의 리스트를 반환한다")
   @GetMapping("/tutor")
-  public ResponseEntity<List<Tutor>> getAllTutor() {
+  public ResponseEntity<?> getAllTutor() {
     List<Tutor> tutorList = tutorRepository.findAll();
-    return new ResponseEntity<List<Tutor>>(tutorList, HttpStatus.OK);
+
+    if(!tutorList.isEmpty()) {
+      return new ResponseEntity<List<Tutor>>(tutorList, HttpStatus.OK);
+    }
+
+    return new ResponseEntity<> (
+        new ErrorMessage(
+            ErrorType.TUTOR_NOT_EXIST.toString(),
+            HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
   }
 
   @ApiOperation(value = "Post New Tutor", notes = "새로운 튜터를 삽입한다")
   @PostMapping("/tutor")
-  public void insertTutor(@RequestBody Tutor newTutor) {
-
-
-    // Test용 코드
-    tutorRepository.deleteAll();
-
-    Date now = new Date();
-
-    //////////////////////// Test용 코드
-    newTutor.setId(ObjectId.getSmallestWithDate(now));
-    newTutor.setPassword("1234");
-    newTutor.setEmail("1234");
-    newTutor.setPhoneNumber("1234");
-    newTutor.setNickname("1234");
-    newTutor.setName("1234");
-    newTutor.setTutorProfile(new Profile("asdf", "bdcf"));
-    List<ObjectId> temp = new ArrayList<>();
-    newTutor.setInstList(temp);
-
-    List<TutorCareer> careerTemp = new ArrayList<>();
-    TutorCareer a = new TutorCareer("asdf","bcdf","phtj");
-    TutorCareer b = new TutorCareer("asfsf","bcdf","phtj");
-    careerTemp.add(a);
-    careerTemp.add(b);
-
-    newTutor.setTutorCareerList(careerTemp);
-
-    List<TutorAward> _award = new ArrayList<TutorAward>();
-    TutorAccount _account = new TutorAccount();
-    List<ClassEntity> tutorsClass = new ArrayList<>();
-
-    newTutor.setTutorAwardList(_award);
-    newTutor.setTutorAccount(_account);
-    newTutor.setTutorClass(tutorsClass);
-
+  public ResponseEntity<?> insertTutor(@RequestBody Tutor newTutor) {
     Tutor tutor = new Tutor(newTutor); // Test용 코드 아래가 진퉁
 
-    /////////////////////////
-    
-
-    // 진퉁
-
-    newTutor.setPassword(UserSha256.encrypt(newTutor.getPassword()));
-    int point = 0;
-    int like = 0;
-
-//    Builder 이용해서 하다가 포기하고 생성자 하나 생성
-//    Tutor tutor = new Tutor(newTutor.getId(), newTutor.getPassword(), newTutor.getEmail(),
-//        newTutor.getPhoneNumber(), newTutor.getNickname(), newTutor.getName(), newTutor.getTutorProfile(),
-//        newTutor.getInstList());
-
-    tutor.setLike(0);
-    tutor.setPoint(0);
-
     tutorRepository.save(tutor);
+
+    return new ResponseEntity<>(tutor, HttpStatus.OK);
   }
 
   @ApiOperation(value = "Get Tutor", notes = "ObjectID로 부터 튜터를 가져와 반환한다")
   @GetMapping("/tutor/{id}")
-  public ResponseEntity<Tutor> getOneTutor(@PathVariable ObjectId id) {
-    System.out.println(id.toString());
-    Tutor tutor = tutorRepository.findById(id).orElseThrow(() -> {
-      return new IllegalArgumentException("해당 사용자는 존재하지 않습니다");
-    });
+  public ResponseEntity<?> getOneTutor(@PathVariable ObjectId id) {
+    Optional<Tutor> tutor = tutorRepository.findById(id);
 
-    return new ResponseEntity<Tutor> (tutor, HttpStatus.OK);
+    if(tutor.isPresent()){
+      return new ResponseEntity<> (tutor.get(), HttpStatus.OK);
+    }
+
+    return new ResponseEntity<> (
+        new ErrorMessage(
+            ErrorType.TUTOR_NOT_EXIST.toString(),
+            HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
   }
 
   @ApiOperation(value = "Update Tutor", notes = "ObjectID로 부터 튜터를 변경한다")
   @PutMapping("/tutor/{id}")
-  public void updateOneTutor(@PathVariable ObjectId id, @RequestBody Tutor newTutor) {
+  public ResponseEntity<?> updateOneTutor(@PathVariable ObjectId id, @RequestBody Tutor newTutor) {
 
-    Tutor tutor = tutorRepository.findById(id).orElseThrow(() -> {
-      return new IllegalArgumentException("해당 사용자는 존재하지 않습니다");
-    });
+    Optional<Tutor> tutor = tutorRepository.findById(id);
 
-    tutor.setName(newTutor.getName());
-    tutor.setNickname(newTutor.getNickname());
-    tutor.setTutorProfile(newTutor.getTutorProfile());
-    tutor.setPhoneNumber(newTutor.getPhoneNumber());
-    tutor.setEmail(newTutor.getEmail());
-    tutor.setPassword(UserSha256.encrypt(newTutor.getPassword()));
+    if(tutor.isPresent()){
+      tutor.get().setName(newTutor.getName());
+      tutor.get().setNickname(newTutor.getNickname());
+      tutor.get().setTutorProfile(newTutor.getTutorProfile());
+      tutor.get().setPhoneNumber(newTutor.getPhoneNumber());
+      tutor.get().setEmail(newTutor.getEmail());
+      tutor.get().setPassword(UserSha256.encrypt(newTutor.getPassword())); // 비밀번호 암호화 어떻게 할지 아직 모름
 
-    // 업데이트 시, 안 들어가는 것이 맞음
-    // tutor.setLike(newTutor.getLike());
-    // tutor.setPoint(newTutor.getName());
+      tutor.get().setLike(newTutor.getLike());
+      tutor.get().setPoint(newTutor.getPoint());
 
-    tutor.setTutorCareerList(removeDuplicateCareerByName(tutor.getTutorCareerList(), newTutor.getTutorCareerList()));
-    tutor.setTutorAwardList(removeDuplicateAwardByName(tutor.getTutorAwardList(), newTutor.getTutorAwardList()));
-    tutor.setTutorAccount(newTutor.getTutorAccount());
-    tutor.setInstList(newTutor.getInstList());
+      tutor.get().setTutorCareerList(removeDuplicateCareerByName(tutor.get().getTutorCareerList(), newTutor.getTutorCareerList()));
+      tutor.get().setTutorAwardList(removeDuplicateAwardByName(tutor.get().getTutorAwardList(), newTutor.getTutorAwardList()));
+      tutor.get().setTutorAccount(newTutor.getTutorAccount());
+      tutor.get().setInstList(newTutor.getInstList());
 
-    tutorRepository.save(tutor);
+      tutorRepository.save(tutor.get());
+
+      return new ResponseEntity<> (tutor.get(), HttpStatus.OK);
+    }
+
+    return new ResponseEntity<> (
+        new ErrorMessage(
+            ErrorType.TUTOR_NOT_EXIST.toString(),
+            HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
   }
 
   @ApiOperation(value = "Delete Tutor", notes = "ObjectID로 부터 튜터를 삭제한다")
   @DeleteMapping("/tutor/{id}")
-  public void deleteTutor(@PathVariable ObjectId id) {
-    tutorRepository.deleteById(id);
+  public ResponseEntity<?> deleteTutor(@PathVariable ObjectId id) {
+    Optional<Tutor> tutor = tutorRepository.findById(id);
+
+    if(tutor.isPresent()) {
+
+      tutorRepository.deleteById(id);
+
+      return new ResponseEntity<> (id.toString() + " delete complete", HttpStatus.OK);
+    }
+
+    return new ResponseEntity<> (
+        new ErrorMessage(
+            ErrorType.TUTOR_NOT_EXIST.toString(),
+            HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
   }
 
   @ApiOperation(value = "Update Password", notes = "튜터의 패스워드를 수정한다")
   @PutMapping("/tutor/{id}/password")
-  public Boolean updatePassword(@PathVariable ObjectId id, String password, String newPassword)
+  public ResponseEntity<?> updatePassword(@PathVariable ObjectId id, String password, String newPassword)
   {
-    Tutor tutor = tutorRepository.findById(id).orElseThrow(() -> {
-      return new IllegalArgumentException("해당 사용자는 존재하지 않습니다");
-    });
+    Optional<Tutor> tutor = tutorRepository.findById(id);
 
-    if(tutor.getPassword().equals(password)) {
-      tutor.setPassword(newPassword);
-      tutorRepository.save(tutor);
-      return true;
+    if(tutor.isPresent()){
+      if(tutor.get().getPassword().equals(password)) {
+        tutor.get().setPassword(newPassword);
+        tutorRepository.save(tutor.get());
+        return new ResponseEntity<> ("password update success", HttpStatus.OK);
+      }
+      else {
+        System.out.println("비밀번호 불일치");
+        return new ResponseEntity<> ("password update fail", HttpStatus.NOT_MODIFIED);
+      }
     }
-    else {
-      System.out.println("비밀번호 불일치");
-      return false;
-    }
+
+    return new ResponseEntity<> (
+        new ErrorMessage(
+            ErrorType.TUTOR_NOT_EXIST.toString(),
+            HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
   }
 
   // Career 중복 제거
@@ -195,102 +191,187 @@ public class TutorController {
     return originalList;
   }
 
-
   @ApiOperation(value = "Get TutorFeedbackTime", notes = "튜터의 피드백 가능 시간을 조회한다")
   @GetMapping("/tutor/{id}/feedback/")
-  public ResponseEntity<List<TutorFeedbackAvailableTime>> GetFeedbackList(@PathVariable ObjectId id, List<Date> feedbackTime) {
+  public ResponseEntity<?> GetFeedbackList(@PathVariable ObjectId id, List<Date> feedbackTime) {
     // 유저의 피드백 조회해서 튜터의 피드백을 Get할 때, reservation을 False 표시 해야하나
     // 유저의 피드백 등록 시에, reservation을 True 표시 해야하나
     // 프론트에서 튜터의 피드백과 유저의 피드백을 get해서 비교하는 것인지
-    Tutor tutor = tutorRepository.findById(id).orElseThrow(() -> {
-      return new IllegalArgumentException("해당 사용자는 존재하지 않습니다");
-    });
+    Optional<Tutor> tutor = tutorRepository.findById(id);
 
-    return new ResponseEntity<List<TutorFeedbackAvailableTime>> (tutor.getTutorFeedbackAvailableTime(), HttpStatus.OK);
+    if(tutor.isPresent()){
+      return new ResponseEntity<> (tutor.get().getTutorFeedbackAvailableTime(), HttpStatus.OK);
+    }
+
+    return new ResponseEntity<> (
+        new ErrorMessage(
+        ErrorType.TUTOR_NOT_EXIST.toString(),
+        HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
   }
 
   @ApiOperation(value = "Post TutorFeedbackTime", notes = "튜터의 피드백 가능 시간을 등록한다")
   @PostMapping("/tutor/{id}/feedback/")
-  public void postFeedbackList(@PathVariable ObjectId id, List<Date> feedbackTime) {
-    Tutor tutor = tutorRepository.findById(id).orElseThrow(() -> {
-      return new IllegalArgumentException("해당 사용자는 존재하지 않습니다");
-    });
+  public ResponseEntity<?> postFeedbackList(@PathVariable ObjectId id, List<Date> feedbackTime) {
+    Optional<Tutor> tutor = tutorRepository.findById(id);
 
-    List<TutorFeedbackAvailableTime> feedbackAvailableTimeList = new ArrayList<>();
-    for(Date date : feedbackTime) {
-      feedbackAvailableTimeList.add(new TutorFeedbackAvailableTime(date, false));
+    if(tutor.isPresent()) {
+      List<TutorFeedbackAvailableTime> feedbackAvailableTimeList = new ArrayList<>();
+      for (Date date : feedbackTime) {
+        feedbackAvailableTimeList.add(new TutorFeedbackAvailableTime(date, false));
+      }
+
+      tutor.get().setTutorFeedbackAvailableTime(feedbackAvailableTimeList);
+
+      tutorRepository.save(tutor.get());
+
+      return new ResponseEntity<>(tutor.get().getTutorFeedbackAvailableTime(), HttpStatus.OK);
     }
 
-    tutor.setTutorFeedbackAvailableTime(feedbackAvailableTimeList);
-
-    tutorRepository.save(tutor);
+    return new ResponseEntity<> (
+        new ErrorMessage(
+            ErrorType.TUTOR_NOT_EXIST.toString(),
+            HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
   }
 
   @ApiOperation(value = "Get TutorPoint", notes = "튜터의 포인트를 조회한다")
   @GetMapping("/tutor/{id}/point/")
-  public ResponseEntity<Integer> getPoint(@PathVariable ObjectId id) {
-    Tutor tutor = tutorRepository.findById(id).orElseThrow(() -> {
-      return new IllegalArgumentException("해당 사용자는 존재하지 않습니다");
-    });
+  public ResponseEntity<?> getPoint(@PathVariable ObjectId id) {
+    Optional<Tutor> tutor = tutorRepository.findById(id);
 
-    return new ResponseEntity<Integer> (tutor.getPoint(), HttpStatus.OK);
+    if(tutor.isPresent()) {
+      return new ResponseEntity<>(tutor.get().getPoint(), HttpStatus.OK);
+    }
+
+    return new ResponseEntity<> (
+        new ErrorMessage(
+            ErrorType.TUTOR_NOT_EXIST.toString(),
+            HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
+
   }
 
   @ApiOperation(value = "Put TutorPoint", notes = "튜터의 포인트를 수정한다")
   @PutMapping("/tutor/{id}/point/")
-  public void putPoint(@PathVariable ObjectId id, Integer point) {
-    Tutor tutor = tutorRepository.findById(id).orElseThrow(() -> {
-      return new IllegalArgumentException("해당 사용자는 존재하지 않습니다");
-    });
+  public ResponseEntity<?> putPoint(@PathVariable ObjectId id, Integer point) {
+    Optional<Tutor> tutor = tutorRepository.findById(id);
 
-    tutor.setPoint(tutor.getPoint() + point);
+    if(tutor.isPresent()) {
 
-    tutorRepository.save(tutor);
+      tutor.get().setPoint(tutor.get().getPoint() + point);
+
+      tutorRepository.save(tutor.get());
+
+      return new ResponseEntity<>(tutor.get().getPoint(), HttpStatus.OK);
+    }
+
+    return new ResponseEntity<> (
+        new ErrorMessage(
+            ErrorType.TUTOR_NOT_EXIST.toString(),
+            HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
   }
 
   @ApiOperation(value = "Get TutorLike", notes = "튜터의 좋아요를 조회한다")
   @GetMapping("/tutor/{id}/like/")
-  public ResponseEntity<Integer> getLike(@PathVariable ObjectId id) {
-    Tutor tutor = tutorRepository.findById(id).orElseThrow(() -> {
-      return new IllegalArgumentException("해당 사용자는 존재하지 않습니다");
-    });
+  public ResponseEntity<?> getLike(@PathVariable ObjectId id) {
+    Optional<Tutor> tutor = tutorRepository.findById(id);
 
-    return new ResponseEntity<Integer> (tutor.getLike(), HttpStatus.OK);
+    if(tutor.isPresent()) {
+      return new ResponseEntity<>(tutor.get().getLike(), HttpStatus.OK);
+    }
+
+    return new ResponseEntity<> (
+        new ErrorMessage(
+            ErrorType.TUTOR_NOT_EXIST.toString(),
+            HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
   }
 
   @ApiOperation(value = "Put TutorLike", notes = "튜터의 좋아요를 수정한다")
   @PutMapping("/tutor/{id}/like/")
-  public void putLike(@PathVariable ObjectId id, Integer like) {
-    Tutor tutor = tutorRepository.findById(id).orElseThrow(() -> {
-      return new IllegalArgumentException("해당 사용자는 존재하지 않습니다");
-    });
+  public ResponseEntity<?> putLike(@PathVariable ObjectId id, Integer like) {
+    Optional<Tutor> tutor = tutorRepository.findById(id);
 
-    tutor.setPoint(tutor.getPoint() + like);
+    if(tutor.isPresent()) {
 
-    tutorRepository.save(tutor);
+      tutor.get().setLike(tutor.get().getLike() + like);
+
+      tutorRepository.save(tutor.get());
+
+      return new ResponseEntity<>(tutor.get().getLike(), HttpStatus.OK);
+    }
+
+    return new ResponseEntity<> (
+        new ErrorMessage(
+            ErrorType.TUTOR_NOT_EXIST.toString(),
+            HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
   }
 
   @ApiOperation(value = "Get TutorAccount", notes = "튜터의 계좌 정보를 조회한다")
   @GetMapping("/tutor/{id}/account/")
-  public ResponseEntity<TutorAccount> getAccount(@PathVariable ObjectId id) {
-    Tutor tutor = tutorRepository.findById(id).orElseThrow(() -> {
-      return new IllegalArgumentException("해당 사용자는 존재하지 않습니다");
-    });
+  public ResponseEntity<?> getAccount(@PathVariable ObjectId id) {
+    Optional<Tutor> tutor = tutorRepository.findById(id);
 
-    return new ResponseEntity<TutorAccount> (tutor.getTutorAccount(), HttpStatus.OK);
+    if(tutor.isPresent()) {
+
+      return new ResponseEntity<>(tutor.get().getTutorAccount(), HttpStatus.OK);
+    }
+
+    return new ResponseEntity<> (
+        new ErrorMessage(
+            ErrorType.TUTOR_NOT_EXIST.toString(),
+            HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
   }
 
   @ApiOperation(value = "Put TutorAccount", notes = "튜터의 계좌 정보를 수정한다")
   @PutMapping("/tutor/{id}/account/")
-  public void putAccount(@PathVariable ObjectId id, TutorAccount newAccount) {
-    Tutor tutor = tutorRepository.findById(id).orElseThrow(() -> {
-      return new IllegalArgumentException("해당 사용자는 존재하지 않습니다");
-    });
+  public ResponseEntity<?> putAccount(@PathVariable ObjectId id, TutorAccount newAccount) {
+    Optional<Tutor> tutor = tutorRepository.findById(id);
 
-    tutor.setTutorAccount(newAccount);
+    if(tutor.isPresent()) {
 
-    tutorRepository.save(tutor);
+      tutor.get().setTutorAccount(newAccount);
+
+      tutorRepository.save(tutor.get());
+    }
+
+    return new ResponseEntity<> (
+        new ErrorMessage(
+            ErrorType.TUTOR_NOT_EXIST.toString(),
+            HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
   }
 
+  @ApiOperation(value = "Get TutorClass", notes = "튜터의 강의 목록을 조회한다")
+  @GetMapping("/tutor/{id}/class")
+  public ResponseEntity<?> getTutorClass(@PathVariable ObjectId id) {
+    Optional<Tutor> tutor = tutorRepository.findById(id);
 
+    if(tutor.isPresent()) {
+
+      return new ResponseEntity<>(tutor.get().getTutorClass(), HttpStatus.OK);
+    }
+
+    return new ResponseEntity<> (
+        new ErrorMessage(
+            ErrorType.TUTOR_NOT_EXIST.toString(),
+            HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
+  }
+
+  @ApiOperation(value = "Put TutorClass", notes = "튜터의 강의 목록을 수정한다")
+  @PutMapping("/tutor/{id}/class")
+  public ResponseEntity<?> putTutorClass(@PathVariable ObjectId id, List<ClassEntity> tutorClass) {
+    Optional<Tutor> tutor = tutorRepository.findById(id);
+
+    if(tutor.isPresent()) {
+
+      tutor.get().setTutorClass(tutorClass);
+
+      tutorRepository.save(tutor.get());
+
+      return new ResponseEntity<>(tutor.get().getTutorClass(), HttpStatus.OK);
+    }
+
+    return new ResponseEntity<>(
+        new ErrorMessage(
+            ErrorType.TUTOR_NOT_EXIST.toString(),
+            HttpStatus.BAD_REQUEST.value()),
+        HttpStatus.BAD_REQUEST);
+  }
 }
