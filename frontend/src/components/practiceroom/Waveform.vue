43bc343d58e57d5e650baa4628c9f2103b3b7c4a
정수림 <smill5059@naver.com>
2021-02-04 17:38:05 +0900

    Create waveform component
    - frontend/package.json: vue-wave-surfer 모듈 추가
    - frontend/src/components/practiceroom: 오디오 파형 컴포넌트 Waveform.vue 추가
    - frontend/src/main.js: vue-wave-surfer 모듈 추가

---
+++ frontend/src/components/practiceroom/Waveform.vue
@@ -0,0 +1,44 @@
+<template>
+  <div style="width:50%">
+      <vue-wave-surfer 
+        ref="waveform"
+        :src="url"
+        :options="options"
+      ></vue-wave-surfer>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['url'],
+    data() {
+        return {
+            options: {
+                barWidth: 2,
+                normalize: true,
+                cursorColor: '#00ff0000',
+                scrollParent: false
+            }
+        }
+    },
+    mounted() {
+        this.player.on('ready', ()=>{
+            console.log('ready');
+        })
+    },
+    computed: {
+        player() {
+            return this.$refs.waveform.waveSurfer;
+        }
+    },
+    methods: {
+        play() {
+            this.player.playPause();
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
