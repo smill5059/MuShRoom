c29841de7d945e728b5fd260ce0fffb657440fa8
정지환 <kkiu1756@gmail.com>
2021-02-04 19:00:45 +0900

    Feat: Player component
    - audio 파일을 재생하는 component

--- frontend/src/views/PracticeRoom.vue
+++ frontend/src/views/PracticeRoom.vue
@@ -1,18 +1,25 @@
 <template>
   <div>
-      <h1>
-          PracticeRoom
-      </h1>
-      
+    <h1>PracticeRoom</h1>
+    <player
+      url="http://i4a105.p.ssafy.io:8080/downloadFile/sample.mp3"
+    ></player>
   </div>
 </template>
 
 <script>
-export default {
+import Player from "../components/practiceroom/Player.vue";
 
-}
+export default {
+  name: "PracticeRoom",
+  components: {
+    Player,
+  },
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style>
-
 </style>
