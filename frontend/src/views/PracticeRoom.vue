ece771b271b612824f0e8e60e40cd68e8a23c853
이건우 <geonwoo0223@gmail.com>
2021-02-04 20:06:08 +0900

    Modify : PracticeRoom.vue 수정

--- frontend/src/views/PracticeRoom.vue
+++ frontend/src/views/PracticeRoom.vue
@@ -1,31 +1,13 @@
 <template>
   <div>
-<<<<<<< HEAD
-    <h1>
-        PracticeRoom
-    </h1>
-    <Metronome />
-=======
     <h1>PracticeRoom</h1>
     <player
       url="http://i4a105.p.ssafy.io:8080/downloadFile/sample.mp3"
     ></player>
->>>>>>> c29841de7d945e728b5fd260ce0fffb657440fa8
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-import Metronome from '@/components/practiceroom/Metronome.vue';
-
-
-export default {
-  name: 'practiceroom',
-  components: {
-    Metronome,
-  }
-}
-=======
 import Player from "../components/practiceroom/Player.vue";
 
 export default {
@@ -37,7 +19,6 @@ export default {
     return {};
   },
 };
->>>>>>> c29841de7d945e728b5fd260ce0fffb657440fa8
 </script>
 
 <style>
