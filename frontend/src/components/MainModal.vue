07f43cf47d3d15cbe07c518653d698b461a6112f
정수림 <smill5059@naver.com>
2021-02-08 20:01:01 +0900

    Add main modal
    - frontend/src/components/: MainModal.vue(모달 component) 추가
    - frontend/src/views/Home.vue: 모달 component 연결

---
+++ frontend/src/components/MainModal.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="registerModal">
+        <v-dialog 
+        v-model="showModal"
+        max-width="500"
+        persistent>
+            <v-card>
+                <v-row>
+                    <label>링크: </label><input ref="link" :value="address">
+                    <v-btn @click="copy">복사</v-btn>
+                </v-row>
+                <v-row>
+                    <v-btn @click="close" :to="{ name: 'PracticeRoom' }">입장하기</v-btn>
+                </v-row>
+            </v-card>
+        </v-dialog>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['showModal'],
+    data() {
+        return {
+            address: "http://mushroom.com/123483750"
+        }
+    },
+    methods: {
+        close() {
+            this.$emit('close', false);
+        },
+        copy() {
+            this.$refs.link.select();
+            document.execCommand("Copy");
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
